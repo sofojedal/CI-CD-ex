5ca8783729621d6697fb67481529bcdca840b770
sofojedal <sofojedal@gmail.com>
2023-08-22 18:03:27 +0200

    Se agrego nueva funcion

---
+++ test_sample.py
@@ -0,0 +1,18 @@
+def primo(numero):
+	if numero <= 1:
+    		return False
+	if numero <= 3:
+    		return True
+	if numero % 2 == 0 or numero % 3 == 0:
+    		return False
+	i = 5
+	while i * i <= numero:
+    		if numero % i == 0 or numero % (i + 2) == 0:
+        			return False
+    		i += 6
+	return True
+
+
+def test_answer():
+	assert primo(13) == True
+
